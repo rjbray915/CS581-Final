6c5e2a51ae69f8883783dcfd57a437a33c74d102
s4mpl <brandanjroachell@att.net>
2023-04-17 16:48:59 -0400

    AABB with collision checking (inefficient)

--- aabb.py
+++ aabb.py
@@ -17,7 +17,7 @@ class AABB(object):
         
     def union(a1: 'AABB',
      a2: 'AABB') -> 'AABB':
-        padding: int = 25
+        padding: int = 25 # seems to do worse with less padding
         lower = Vector2(0, 0)
         upper = Vector2(0, 0)
         lower.x = min(a1._lower_bound.x, a2._lower_bound.x) - padding
@@ -94,10 +94,10 @@ class AABBNode(object):
         
         if self._is_leaf:
             self._bounding_box.render(surface, pygame.Color(0, 255, 0))
-            surface.blit(cost_font.render(str(int(self._indx)), 1, pygame.Color("coral")), (self._bounding_box._lower_bound.x + 5, self._bounding_box._lower_bound.y + 5))
+            #surface.blit(cost_font.render(str(int(self._indx)), 1, pygame.Color("coral")), (self._bounding_box._lower_bound.x, self._bounding_box._lower_bound.y))
         else:
             self._bounding_box.render(surface, color)
-            surface.blit(cost_font.render(str(int(self.cost())), 1, pygame.Color("coral")), (self._bounding_box._lower_bound.x + 5, self._bounding_box._lower_bound.y + 5))
+            #surface.blit(cost_font.render(str(int(self.cost())), 1, pygame.Color("coral")), (self._bounding_box._lower_bound.x + 5, self._bounding_box._lower_bound.y + 5))
     
 class AABBTree(object):
     _nodes: List[AABBNode]
@@ -115,9 +115,9 @@ class AABBTree(object):
             return
     
         # otherwise, make a new internal node and put this on right
-        best_node: AABBNode = self.find_best_node(self._root, new_node)
+        best_node: AABBNode = self.find_best_node_heuristic(self._root, new_node)
         # internal_node_bb = AABB.union(new_node._bounding_box, best_node._bounding_box)
-        internal_node = AABBNode(False, len(self._nodes))
+        internal_node = AABBNode(False, -1)
         old_node = best_node._parent
         internal_node._parent = old_node
         
@@ -144,7 +144,7 @@ class AABBTree(object):
             curr_node = curr_node._parent
         
     def find_best_node(self, curr_node: AABBNode, new_node: AABBNode) -> AABBNode:
-        print(curr_node._indx)
+        #print(curr_node._indx)
         # if this is a leaf, return
         if not curr_node._left_child and not curr_node._right_child:
             return curr_node
@@ -169,20 +169,31 @@ class AABBTree(object):
             return left_node
         else:
             return right_node
-            
-        # left_cost = sys.maxsize if not curr_node._left_child else curr_node._left_child.cost()
-        # right_cost = sys.maxsize if not curr_node._right_child else curr_node._right_child.cost()
-        # if left_cost < right_cost:
-        #     return self.find_best_node(curr_node._left_child, new_node)
-        # else:
-        #     return self.find_best_node(curr_node._right_child, new_node)
+
+    def find_best_node_heuristic(self, curr_node: AABBNode, new_node: AABBNode) -> AABBNode:
+        # if this is a leaf, return
+        if not curr_node._left_child and not curr_node._right_child:
+            return curr_node
+        
+        # go down the rabbit hole
+        left_cost = sys.maxsize
+        right_cost = sys.maxsize
+        if curr_node._left_child:
+            left_cost = curr_node._left_child.cost() + AABB.union(curr_node._left_child._bounding_box, new_node._bounding_box)._cost
+        if curr_node._right_child:
+            right_cost = curr_node._right_child.cost() + AABB.union(curr_node._right_child._bounding_box, new_node._bounding_box)._cost
+        
+        if left_cost < right_cost:
+            return self.find_best_node(curr_node._left_child, new_node)
+        else:
+            return self.find_best_node(curr_node._right_child, new_node)
 
     def update_tree(self, circles):
         #circles.sort(key=lambda c: (c._pos.x, c._pos.y))
 
         new_tree = AABBTree()
-        for circle in circles:
-            new_node = AABBNode(is_leaf=True, indx=len(new_tree._nodes), rect=circle.rect)
+        for i, circle in enumerate(circles):
+            new_node = AABBNode(is_leaf=True, indx=i, rect=circle.rect)
             new_tree.insert_node(new_node)
 
         return new_tree
@@ -259,8 +270,8 @@ if __name__ == "__main__":
                                 (0, 0),# (random.randint(-20, 20), random.randint(-20, 20)),
                                 radius, 
                                 random.choice(["green", "blue", "yellow", "red", "grey"]))
+            new_node = AABBNode(is_leaf=True, indx=len(circles), rect=curr_circle.rect)
             circles.append(curr_circle)
-            new_node = AABBNode(is_leaf=True, indx=len(aabb_tree._nodes), rect=curr_circle.rect)
             aabb_tree.insert_node(new_node)
             curr_x += radius + spacing
 
@@ -288,6 +299,28 @@ if __name__ == "__main__":
 
         for i in range(len(circles)):
             circles[i].on_tick(dt)
+
+        # determine which AABBs can collide
+        for i, circle in enumerate(circles):
+            stack = [aabb_tree._root]
+            rect1 = circle.rect
+            while len(stack) > 0:
+                top = stack.pop()
+                if top._is_leaf and top._indx != i:
+                    # handle collision
+                    if circle.is_colliding_circle(circles[top._indx]):
+                        circle.reflect_obj(circles[top._indx], dt)
+                else:
+                    if top._left_child:
+                        rect2 = top._left_child._bounding_box
+                        overlapping = not (rect1.x + rect1.w < rect2._lower_bound.x or rect1.x > rect2._upper_bound.x or rect1.y + rect1.h < rect2._lower_bound.y or rect1.y > rect2._upper_bound.y)
+                        if overlapping:
+                            stack.append(top._left_child)
+                    if top._right_child:
+                        rect2 = top._right_child._bounding_box
+                        overlapping = not (rect1.x + rect1.w < rect2._lower_bound.x or rect1.x > rect2._upper_bound.x or rect1.y + rect1.h < rect2._lower_bound.y or rect1.y > rect2._upper_bound.y)
+                        if overlapping:
+                            stack.append(top._right_child)
         
         for circle in circles:
             if circle._pos.x - circle._radius < 0:
@@ -306,7 +339,7 @@ if __name__ == "__main__":
             circle.render(screen)
         
         aabb_tree = aabb_tree.update_tree(circles)
-        aabb_tree.render_tree(screen, pygame.Color(255, 0, 0))
+        aabb_tree.render_tree(screen, pygame.Color(255, 0, 0)) # has little to no effect on framerate
 
         screen.blit(update_fps(), (5, 10))
         pygame.display.flip()
